f585b5ddaa1f304f0a0f6594d5b4ef5ba1193ae2
SergeySokolov-20 <72944422+SergeySokolov-20@users.noreply.github.com>
2020-10-19 19:20:26 +0300

    Pet

--- js/main.js
+++ js/main.js
@@ -1,1 +1,92 @@
-document.querySelector(".menu-icon").onclick=function(){document.querySelector(".menu-icon").classList.toggle("menu-icon-active"),document.querySelector(".header__menu").classList.toggle("menu-active"),document.querySelector("body").classList.toggle("lock")},$(document).ready(function(){$(".slider-container").slick({infinite:!0,slidesToShow:3,slidesToScroll:3,autoplay:!0,autoplaySpeed:2e3,prevArrow:'<img src="img/left1.png" class="slick-prev" alt="prev">',nextArrow:'<img src="img/right1.png" class"slick-next" alt="next">',responsive:[{breakpoint:1024,settings:{slidesToShow:2,slidesToScroll:2,infinite:!0}},{breakpoint:768,settings:{slidesToShow:1,slidesToScroll:1}},{breakpoint:480,settings:{slidesToShow:1,slidesToScroll:1,arrows:!1}}]})});let animItems=document.querySelectorAll("._anim-items");if(animItems.length>0){function animOnScroll(){for(let e=0;e<animItems.length;e++){const t=animItems[e],n=t.offsetHeight,i=offset(t).top,o=4;let s=window.innerHeight-n/o;n>window.innerHeight&&(s=window.innerHeight-window.innerHeight/o),pageYOffset>i-s&&pageYOffset<i+n?t.classList.add("_active"):t.classList.contains("._anim-no-hide")||t.classList.remove("_active")}}function offset(e){const t=e.getBoundingClientRect(),n=window.pageXOffset||document.documentElement.scrollLeft,i=window.pageYOffset||document.documentElement.scrollTop;return{top:t.top+i,left:t.left+n}}window.addEventListener("scroll",animOnScroll),setTimeout(()=>{animOnScroll()},300)}
+document.querySelector('.menu-icon').onclick = function () {
+   document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+   document.querySelector('.header__menu').classList.toggle('menu-active');
+   document.querySelector('body').classList.toggle('lock');
+}
+
+document.querySelector('.header__list').onclick = function () {
+   document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+   document.querySelector('.header__menu').classList.toggle('menu-active');
+   document.querySelector('body').classList.remove('lock');
+}
+
+
+
+$(document).ready(function () {
+   $('.slider-container').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      prevArrow: '<img src="img/left1.png" class="slick-prev" alt="prev">',
+      nextArrow: '<img src="img/right1.png" class"slick-next" alt="next">',
+      responsive: [
+         {
+            breakpoint: 1024,
+            settings: {
+               slidesToShow: 2,
+               slidesToScroll: 2,
+               infinite: true,
+            }
+         },
+         {
+            breakpoint: 768,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1
+            }
+         },
+         {
+            breakpoint: 480,
+            settings: {
+               slidesToShow: 1,
+               slidesToScroll: 1,
+               arrows: false,
+            }
+         },
+      ]
+  });
+});
+
+
+
+let animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+   window.addEventListener('scroll', animOnScroll);
+   function animOnScroll() {
+      for (let index = 0; index < animItems.length; index++) {
+         const animItem = animItems[index];
+         const animItemHeight = animItem.offsetHeight;
+         const animItemOffset = offset(animItem).top;
+         const animStart = 4;
+
+         let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+         if (animItemHeight > window.innerHeight) {
+            animItemPoint = window.innerHeight - window.innerHeight / animStart;
+         }
+
+         if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+            animItem.classList.add('_active');
+         } else {
+            if (!animItem.classList.contains('._anim-no-hide')) {
+               animItem.classList.remove('_active');
+            }
+
+         }
+      }
+   }
+   function offset(el) {
+      const rect = el.getBoundingClientRect(),
+         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+   }
+   setTimeout(() => {
+      animOnScroll();
+   }, 300);
+}
+
+document.addEventListener('touchstart', onTouchStart, { passive: true });
